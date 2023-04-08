c4bf77339283c3900a318347cffe86e72744930b
ElvisAns <ansimapersic@gmail.com>
2023-04-08 02:01:11 +0200

    feat : get movies from our request handler

--- src/requests/index.js
+++ src/requests/index.js
@@ -1,13 +1,19 @@
 import axios from 'axios'
-var _ = require('lodash');
+import _ from 'lodash'
 
 const api_key = import.meta.env.VITE_API_KEY
 
-const movieList = async function(page=1){
-    const response  = await axios.get(`http://www.omdbapi.com/?apikey=${api_key}&type=movie&y=2023&s=movie&page=${page}`)
-    return _.shuffle(response.search);
+const movieList = async function (page = 1) {
+  try {
+    const response = await axios.get(
+      `http://www.omdbapi.com/?apikey=${api_key}&type=movie&y=2023&s=movie&page=${page}`
+    )
+    const { data } = response
+    const { Search } = data
+    return _.shuffle(Search)
+  } catch (error) {
+    throw new Error(error)
+  }
 }
 
-export {
-    movieList
-}
+export { movieList }
